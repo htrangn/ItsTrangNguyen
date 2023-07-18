c7fb1abc76991e4ee0ea8865c9627d5ce529c506
Nguyen H. Trang <124483392+htrangn@users.noreply.github.com>
2023-07-18 10:54:23 +0700

    Update projectMC4AI.py

--- projectMC4AI.py
+++ projectMC4AI.py
@@ -4,6 +4,28 @@ import cv2
 import numpy as np
 from PIL import Image
 
+import base64
+
+main_bg = "sample.jpg"
+main_bg_ext = "jpg"
+
+side_bg = "sample.jpg"
+side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 filename = 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(filename)
 

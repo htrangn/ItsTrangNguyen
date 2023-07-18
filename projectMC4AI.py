12163cef5c916352f0cea1b702343147fadcb512
Nguyen H. Trang <124483392+htrangn@users.noreply.github.com>
2023-07-18 10:59:19 +0700

    Update projectMC4AI.py

--- projectMC4AI.py
+++ projectMC4AI.py
@@ -16,10 +16,10 @@ st.markdown(
     f"""
     <style>
     .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+        background: url(data:image/{logo};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
     }}
    .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+        background: url(data:image/{logo};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
     }}
     </style>
     """,

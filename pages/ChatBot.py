f2e8fd9fd9210cde957c57dcee0f7c57da2352ab
Nguyen H. Trang <124483392+htrangn@users.noreply.github.com>
2023-07-31 18:09:11 +0700

    Update ChatBot.py

--- pages/ChatBot.py
+++ pages/ChatBot.py
@@ -4,6 +4,11 @@ import streamlit as st
 activities2 = ["Chat", "About"]
 choice = st.sidebar.selectbox("Select Activity", activities2)
 
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/htrangn/ItsTrangNguyen/blob/main/pages/Chatbot.py)"
+
 if choice == "Chat":
     st.title("💬 Chatbot")
     if "messages" not in st.session_state:

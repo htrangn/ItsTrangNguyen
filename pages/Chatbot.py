ceaef94ffe8e46f21144f2b89517142609132289
Nguyen H. Trang <124483392+htrangn@users.noreply.github.com>
2023-07-31 17:34:19 +0700

    Update Chatbot.py

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -1,6 +1,8 @@
 import openai
 import streamlit as st
 
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"

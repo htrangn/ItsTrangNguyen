a80b5eee03fa84bafc3787459586c76c91da6dce
Nguyen H. Trang <124483392+htrangn@users.noreply.github.com>
2023-08-12 22:18:55 +0700

    Update Face_Detection_App.py

--- Face_Detection_App.py
+++ Face_Detection_App.py
@@ -25,7 +25,7 @@ def detect_faces(our_image):
 
 def main():
     #Face detection App
-    st.title("Face Detection App")
+    st.title("FACE DETECTION APP")
     with st.sidebar:
         activities = ["Detection", "About"]
         choice = st.sidebar.selectbox("Select Activity", activities)

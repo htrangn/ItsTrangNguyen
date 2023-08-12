c7c8fea4047d0c71b3fdbac4012623ba68a0b63e
Nguyen H. Trang <124483392+htrangn@users.noreply.github.com>
2023-08-12 21:57:08 +0700

    Update Face_Detection_App.py

--- Face_Detection_App.py
+++ Face_Detection_App.py
@@ -29,7 +29,8 @@ def main():
     with st.sidebar:
         activities = ["Detection", "About"]
         choice = st.sidebar.selectbox("Select Activity", activities)
-        "[View the source code](https://github.com/htrangn/ItsTrangNguyen/blob/main/Face_Detection_App.py)" 
+        st.write("View the source code")
+        "[GITHUB](https://github.com/htrangn/ItsTrangNguyen/blob/main/Face_Detection_App.py)" 
 
     #Detection
     if choice == "Detection":
